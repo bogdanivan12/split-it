0592ca4e1f517cff5526b55d32f07cbfa5003935
Bogdan Ivan <bogdivan12@gmail.com>
2024-11-04 02:33:52 +0200

    feat: Add initial data models

---
+++ backend/common/models.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+
+class User(BaseModel):
+    id: str
+    username: str
+    password_hash: str
+    email: str
+    full_name: str = ""
+    group_ids: List[str] = []
+
+
+class Group(BaseModel):
+    id: str
+    name: str
+    description: str = ""
+    owner_id: str
+    member_ids: List[str] = []
+    bill_ids: List[str] = []
+
+
+class InitialPayer(BaseModel):
+    user_id: str
+    amount: float
+
+
+class Product(BaseModel):
+    name: str
+    price: float
+    quantity: int = 1
+    assigned_payer_ids: List[str] = []
+
+
+class Bill(BaseModel):
+    id: str
+    group_id: str
+    name: str
+    description: str = ""
+    initial_payers: List[InitialPayer] = []
+    amount: float
+    date: datetime
+    # if member_ids is None, the bill is split by products
+    member_ids: Optional[List[str]] = []
+    # if products is None, the bill is split evenly by members
+    products: Optional[List[Product]] = None
+    payment_ids: List[str] = []
+
+
+class Payment(BaseModel):
+    id: str
+    bill_id: str
+    amount: float
+    payer_id: str
+    recipient_id: str
+    date: datetime
+    method: str = "not selected"  # e.g., "cash", "revolut" etc.
+    status: str = "not started"  # e.g., "in progress", "completed" etc.
