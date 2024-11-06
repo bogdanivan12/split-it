9a16c8a48c5d3085fb4464a33e11e8d358b558f9
Bogdan Ivan <bogdivan12@gmail.com>
2024-11-07 00:22:49 +0200

    fix(api): Updated max_length for password from 20 to 128

--- backend/api/api_request_classes.py
+++ backend/api/api_request_classes.py
@@ -4,4 +4,4 @@ from backend.common import models
 
 
 class CreateUserRequest(models.User):
-    password: str = Field(min_length=8, max_length=20)
+    password: str = Field(min_length=8, max_length=128)
