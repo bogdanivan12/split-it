f78543db7e21cfb5612255fd137faacbb977261e
Octavian Gaina <gainaoctavian@gmail.com>
2024-11-12 00:38:01 +0200

    fix(api): Use update method on dictionaries instead of dictionaries comprehension

--- backend/api/api_request_classes.py
+++ backend/api/api_request_classes.py
@@ -11,6 +11,6 @@ class CreateGroupRequest(BaseModel):
 class UpdateGroupRequest(BaseModel):
     name: Optional[str] = Field(min_length=5, max_length=30)
     description: Optional[str] = Field(max_length=100)
-    owner_id: Optional[PydanticObjectId]
+    owner_id: Optional[PydanticObjectId] = Field(default=None)
     member_ids: Optional[List[PydanticObjectId]] = Field(default_factory=list)
     bill_ids: Optional[List[PydanticObjectId]] = Field(default_factory=list)
